18c49ccec54011c7d79d48e67358772fc4957b84
Tony <tonyeen@163.com>
2022-04-10 06:17:29 +0800

    simple components

--- libs/components/src/lib/forms/index.ts
+++ libs/components/src/lib/forms/index.ts
@@ -4,3 +4,8 @@ export * from './form-control.widget';
 export * from './form-controls-registry';
 
 export * from './input.widget';
+export * from './input-number.widget';
+export * from './select.widget';
+export * from './range-picker-widget.component';
+export * from './user-lookup.widget';
+export * from './org-unit-lookup.widget';
--- libs/components/src/lib/simple-table/index.ts
+++ libs/components/src/lib/simple-table/index.ts
@@ -1,3 +1,6 @@
 export * from './simple-table.module';
 
 export * from './simple-table/simple-table.component';
+export * from './col-tpl.directive';
+export * from './cell-value.directive';
+export * from './simple-table.typings';
