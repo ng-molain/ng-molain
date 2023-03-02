e114dd0f34873a07979067bf1d552932835d5473
Tony <tonyeen@163.com>
2023-03-02 10:14:22 +0800

    添加BaseApi 路径处理，支持参数模式

--- libs/common/src/lib/api/base.api.ts
+++ libs/common/src/lib/api/base.api.ts
@@ -1,33 +1,48 @@
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
 import {Page, Pageable, Pagination} from "@ng-molain/components";
+import {ParamMap, subst} from "urlcat";
+
+export interface ApiOptions {
+  pathVariables?: ParamMap;
+}
 
 export abstract class BaseApi<M, ID> {
   protected abstract readonly httpClient: HttpClient;
+  /**
+   * 支持路径参数，参考 ApiOptions.pathVariables
+   * 使用 urlcat（https://urlcat.org/）来处理路径参数
+   * @protected
+   */
   protected abstract readonly baseUrl: string;
 
-  pageList(params: any & Pageable = {}): Observable<Pagination<M>> {
-    return this.httpClient.get<Pagination<M>>(`${this.baseUrl}`, {params}).pipe(
+  protected getBaseUrl(options?: ApiOptions) {
+    const params = (options?.pathVariables) ?? {};
+    return subst(this.baseUrl, params);
+  }
+
+  pageList(params: any & Pageable = {}, options?: ApiOptions): Observable<Pagination<M>> {
+    return this.httpClient.get<Pagination<M>>(`${this.getBaseUrl(options)}`, {params}).pipe(
       map(value => Page.of(value))
     );
   }
 
-  getById(id: ID): Observable<M> {
-    return this.httpClient.get<M>(`${this.baseUrl}/${id}`);
+  getById(id: ID, options?: ApiOptions): Observable<M> {
+    return this.httpClient.get<M>(`${this.getBaseUrl(options)}/${id}`);
   }
 
-  create(payload: Partial<M>): Observable<M> {
-    return this.httpClient.post<M>(`${this.baseUrl}`, payload);
+  create(payload: Partial<M>, options?: ApiOptions): Observable<M> {
+    return this.httpClient.post<M>(`${this.getBaseUrl(options)}`, payload);
   }
 
-  updateById(id: ID, payload: Partial<M>): Observable<M> {
-    return this.httpClient.put<M>(`${this.baseUrl}/${id}`, payload);
+  updateById(id: ID, payload: Partial<M>, options?: ApiOptions): Observable<M> {
+    return this.httpClient.put<M>(`${this.getBaseUrl(options)}/${id}`, payload);
   }
 
-  delete(id: ID | ID[]): Observable<void> {
+  delete(id: ID | ID[], options?: ApiOptions): Observable<void> {
     const params: any = {
       ids: Array.isArray(id) ? id : [id]
     };
-    return this.httpClient.delete<void>(`${this.baseUrl}`, {params});
+    return this.httpClient.delete<void>(`${this.getBaseUrl(options)}`, {params});
   }
 }
