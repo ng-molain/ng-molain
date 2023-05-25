74ade99bd8e90c7c05f633af0b40ad7503442d83
Tony <tonyeen@163.com>
2023-05-25 14:23:46 +0800

    Utils

--- libs/components/src/lib/download/download-progress/download-progress.component.ts
+++ libs/components/src/lib/download/download-progress/download-progress.component.ts
@@ -8,10 +8,10 @@ import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
         <div>{{data.name}}</div>
         <nz-progress *ngIf="data.total" [nzPercent]="data?.percent" [nzShowInfo]="false"></nz-progress>
         <div class="space-x-3">
-          <span>已下载：</span>
+          <span>已下载:</span>
           <span>{{data.loaded | nzBytes }}</span>
           <ng-container *ngIf="data.total">
-            <span class="ml-6">共：</span>
+            <span class="ml-8">共:</span>
             <span>{{data.total | nzBytes }}</span>
           </ng-container>
         </div>
