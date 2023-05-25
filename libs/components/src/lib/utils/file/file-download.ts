7bd7b2ddcd608e3173996cc3a97a45ef212e7ae2
Tony <tonyeen@163.com>
2023-05-25 11:34:38 +0800

    Utils

--- libs/components/src/lib/utils/file/file-download.ts
+++ libs/components/src/lib/utils/file/file-download.ts
@@ -1,4 +1,4 @@
-import {HttpResponse} from "@angular/common/http";
+import {HttpHeaders, HttpResponse} from "@angular/common/http";
 
 export function saveAs(blob: Blob, fileName: string) {
   const a = document.createElement('a');
@@ -9,10 +9,23 @@ export function saveAs(blob: Blob, fileName: string) {
   URL.revokeObjectURL(url);
 }
 
+// https://blog.csdn.net/qq_40044912/article/details/110475876
 export function download(response: HttpResponse<Blob>) {
   const blob = response.body;
-  const fileName = response.headers.get('downloadName') ?? Math.random().toString();
+  const fileName = tryGetFilename(response.headers);
   if (blob) {
     saveAs(blob, decodeURI(fileName));
   }
+}
+
+export function tryGetFilename(headers: HttpHeaders) {
+  const contentDisposition = headers.get("Content-Disposition");
+  if (contentDisposition) {
+    const tmpStr = contentDisposition?.split("filename*=")[1];
+    const filename = decodeURI(tmpStr.split("'")[2]);
+    if (filename) {
+      return filename;
+    }
+  }
+  return headers.get('downloadName') ?? Math.random().toString();
 }
