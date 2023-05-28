a7c265229f9870229e9099717da320b3ada91ede
Tony <tonyeen@163.com>
2023-05-28 17:09:38 +0800

    user lookup

--- libs/components/src/lib/user-lookup/index.ts
+++ libs/components/src/lib/user-lookup/index.ts
@@ -3,4 +3,6 @@ export * from './user-lookup/user-lookup.component';
 export * from './user-lookup-dialog/user-lookup-dialog.component';
 export * from './user-lookup.service';
 
+export * from './user-lookup.typings';
+
 export * from './user-lookup.module';
